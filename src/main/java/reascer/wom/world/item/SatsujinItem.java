492b219d11a9d7a4ae526c7766b66127d3b7bbc1
BiakCeu <88417452+BiakCeu@users.noreply.github.com>
2023-10-27 21:11:48 -0400

    Update SatsujinItem.java

--- src/main/java/reascer/wom/world/item/SatsujinItem.java
+++ src/main/java/reascer/wom/world/item/SatsujinItem.java
@@ -41,7 +41,7 @@ public class SatsujinItem extends WeaponItem {
 			this.tooltipExpand.add(Component.translatable("item." + WeaponsOfMinecraft.MODID + ".satsujin.tooltip"));
 		}
 		
-		this.attackDamage = 6.0F;
+		this.attackDamage = 16.0F;
 		this.attackSpeed = -1.8F;
 	}
 	
